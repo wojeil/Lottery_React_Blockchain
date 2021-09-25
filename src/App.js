d42ec6d5ccb5b090ef421a89bfe2ab2b1c338304
Wadih Ojeil <wadih.ojeil@gmail.com>
2021-09-25 18:10:02 -0400

    added the Balance and number of players

--- src/App.js
+++ src/App.js
@@ -5,14 +5,21 @@ import web3 from "./web3";
 import lottery from "./lottery";
 
 class App extends React.Component {
-  constructor(props){
-    super(props);
-    this.state ={manager:''};
-  }
+    state ={
+      manager: '',
+      players: [],
+      balance: ''
+    };
  async componentDidMount(){
+   //getting the manager from the network
     const manager = await lottery.methods.manager().call();
+  //getting the players from the network
+  const players = await lottery.methods.getPlayers().call();
+  //getting the Balance of the Lottery
+  const balance = await web3.eth.getBalance(lottery.options.address);
 
-    this.setState({manager});
+
+    this.setState({manager, players, balance});
   }
   render() {
     // console.log(web3);
@@ -20,7 +27,9 @@ class App extends React.Component {
     return (
       <div>
         <h2>Lottery Contract</h2>
-        <p>This contract is managed by {this.state.manager}</p>
+        <p>This contract is managed by {this.state.manager}.
+        There are currently {this.state.players.length} people enrolled.
+        The Prize Pool contains {web3.utils.fromWei(this.state.balance, 'ether')} ether!!!</p>
       </div>
      
     );
