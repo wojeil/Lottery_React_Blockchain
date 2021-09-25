2d9c8a2531359c00dd2d75476e60f17c2bf264c8
Wadih Ojeil <wadih.ojeil@gmail.com>
2021-09-25 13:43:32 -0400

    set up lottery.js and inserted a local copy of my contract in the Ethereum blockchain that will act as a portal

---
+++ src/lottery.js
@@ -0,0 +1,8 @@
+import web3 from './web3';
+
+const address = '0xAE1F0e80f714bCf2603617fFB4647483e428fd1B';
+
+const abi = [{"constant":true,"inputs":[],"name":"manager","outputs":[{"name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[],"name":"pickWinner","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"getPlayers","outputs":[{"name":"","type":"address[]"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[],"name":"enter","outputs":[],"payable":true,"stateMutability":"payable","type":"function"},{"constant":true,"inputs":[{"name":"","type":"uint256"}],"name":"players","outputs":[{"name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"inputs":[],"payable":false,"stateMutability":"nonpayable","type":"constructor"}];
+
+//portal to contract that exists on the BlockChain ..local copy
+export default new web3.eth.Contract(abi, address);
